7c76e2f8b0fa06c4a783ac62c53e91ddae3fb072
Andrew Mercer <diddydodge27@yahoo.com>
2015-03-22 21:15:59 -0400

    This time for sure

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,10 +1,6 @@
 class PollsController < ApplicationController
   before_action :authenticate_user_from_token!
   
-  has_attached_file :photo_post, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
-                                              :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :photo_post, :content_type => /\Aimage\/.*\Z/
-
   def create
     @user = current_user
     @poll = @user.polls.new(poll_params)
