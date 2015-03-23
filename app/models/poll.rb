fd06f9b53472a313d185fb337c1a6ccaf750de04
Andrew Mercer <diddydodge27@yahoo.com>
2015-03-22 21:11:55 -0400

    oops. Model, not Controller. Rookie mistake

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,4 +1,7 @@
 class Poll < ActiveRecord::Base
-  require 'paperclip'
   belongs_to :user
+  has_attached_file :photo_post, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+                                              :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :photo_post, :content_type => /\Aimage\/.*\Z/
+
 end
