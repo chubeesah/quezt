f4817fe564b9ef7fa396c11723bb2457a2a0121b
Andrew Mercer <diddydodge27@yahoo.com>
2015-03-28 17:46:25 -0400

    minor change to be changed later

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -69,6 +69,6 @@ class PollsController < ApplicationController
   def poll_params
     params.require(:poll).permit(:question, :photo_post, :answer_1, 
                                  :answer_2, :answer_3, :answer_4, :vote_1, 
-                                 :vote_2, :vote_3, :vote_4, :latitude, :longitude)                            
+                                 :vote_2, :vote_3, :vote_4)                            
   end
 end
