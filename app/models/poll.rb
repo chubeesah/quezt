4408ac9cf917e353b5df1273e1d74d777b2dd5de
Andrew Mercer <diddydodge27@yahoo.com>
2015-04-02 18:53:36 -0400

    oops

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -43,20 +43,19 @@ class Poll < ActiveRecord::Base
   end 
 
   protected
-    def decode_base64_image
-      if image_data && content_type && original_filename
-        decoded_data = Base64.decode64(image_data)
- 
-        data = StringIO.new(decoded_data)
-        data.class_eval do
-          attr_accessor :content_type, :original_filename
-        end
- 
-        data.content_type = content_type
-        data.original_filename = File.basename(original_filename)
- 
-        self.image = data
+  def decode_base64_image
+    if image_data && content_type && original_filename
+      decoded_data = Base64.decode64(image_data)
+
+      data = StringIO.new(decoded_data)
+      data.class_eval do
+        attr_accessor :content_type, :original_filename
       end
+
+      data.content_type = content_type
+      data.original_filename = File.basename(original_filename)
+
+      self.image = data
     end
-end
+  end
 end
