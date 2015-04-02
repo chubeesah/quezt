0ce03ff3aebb39eed6bc3a1f1c95b72bbfc0fe7c
Andrew Mercer <diddydodge27@yahoo.com>
2015-04-02 18:46:46 -0400

    test

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -7,6 +7,8 @@ class Poll < ActiveRecord::Base
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode
 
+  before_save :decode_base64_image
+
   def vote_for_1(poll)  
     vote1 = poll.vote_1.to_i
     if self.answer_1 
@@ -30,7 +32,7 @@ class Poll < ActiveRecord::Base
     end
     poll.update(vote_3: vote3)
   end 
-  
+
   def vote_for_4(poll)
     vote4 = poll.vote_4.to_i
     if self.answer_4
@@ -38,4 +40,22 @@ class Poll < ActiveRecord::Base
     end
     poll.update(vote_4: vote4)
   end 
+
+  protected
+    def decode_base64_image
+      if image_data && content_type && original_filename
+        decoded_data = Base64.decode64(image_data)
+ 
+        data = StringIO.new(decoded_data)
+        data.class_eval do
+          attr_accessor :content_type, :original_filename
+        end
+ 
+        data.content_type = content_type
+        data.original_filename = File.basename(original_filename)
+ 
+        self.image = data
+      end
+    end
+end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   before_save :ensure_authentication_token
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+                                          :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
   has_many :polls
 
-  def to_param
-    username || user_id
-  end
+  before_save :decode_base64_image
 
   def ensure_authentication_token
     if authentication_token.blank?
@@ -30,6 +29,23 @@ class User < ActiveRecord::Base
     loop do
       token = Devise.friendly_token
       break token unless User.where(authentication_token: token).first
+    end
+  end
+
+  protected
+  def decode_base64_image
+    if image_data && content_type && original_filename
+      decoded_data = Base64.decode64(image_data)
+
+      data = StringIO.new(decoded_data)
+      data.class_eval do
+        attr_accessor :content_type, :original_filename
+      end
+
+      data.content_type = content_type
+      data.original_filename = File.basename(original_filename)
+
+      self.image = data
     end
   end
 end
